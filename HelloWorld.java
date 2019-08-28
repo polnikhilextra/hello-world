7ce87df665e12d86910de3558e774c54a9138c08
polnikhilextra <53988262+polnikhilextra@users.noreply.github.com>
2019-08-28 15:23:39 +0000

    Update HelloWorld.java
    print statement updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld{
 
 	public static void main(String[] args){
 
-			System.out.println("Hello World, Hi I am Nikhil Pol...");
+			System.out.println("Hello!, This is test program for Git + Jenkins Integration.");
 
 
 	}
